e0efaf956d3a75c466cf10949d99ab2f8514a9ab
taygun08 <37046560+taygun08@users.noreply.github.com>
2021-09-12 18:35:46 +0300

    Update stop-loss.py

--- stop-loss.py
+++ stop-loss.py
@@ -1,5 +1,8 @@
 # atr değerini baz alaraktan stop-loss hesaplamak için hızlı bir yol. hesaplanan değerin biraz daha altına koymanızı öneririm.
 # atr tradingview indikatör kısmına yazarsanız ortalama gerçek aralık diye karşınıza çıkar.
+# kendi kullandığım yöntemdir.
+# stop tavsiyesi değildir :D
+
 print("Created By Taygun Fırıncı")
 buy = float(input("alım sinyali noktasını giriniz: "))
 ath = float(input("atr değerini giriniz: "))
